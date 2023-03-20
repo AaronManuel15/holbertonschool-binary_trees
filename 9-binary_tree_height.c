fa75b064e6e07ca2d47992007052ca62efd28aba
AaronManuel15 <aaronmanuel15@gmail.com>
2023-03-20 17:53:51 -0500

    Task 9: changed to recursion to compare both branches of the root node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,21 +11,16 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int height = 0;
+	int left_height = 0;
+	int right_height = 0;
 
 	if (tree == NULL)
 		return (0);
-	while (tree->left != NULL || tree->right != NULL)
-	{
-		if (tree->left != NULL)
-		{
-			tree = tree->left;
-			height++;
-		}
-		else if (tree->right != NULL)
-		{
-			tree = tree->right;
-			height++;
-		}
-	}
-	return (height);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	if (left_height > right_height)
+		height = left_height;
+	else
+		height = right_height;
+	return (height + 1);
 }
