a9968a197f7f7e861bde73c79734a4f95eb8c512
AaronManuel15 <aaronmanuel15@gmail.com>
2023-03-20 18:17:17 -0500

    Added check for leaf node to not add extra length

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,17 +10,14 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int height = 0;
-	int left_height = 0;
-	int right_height = 0;
+	int left_height;
+	int right_height;
 
-	if (tree == NULL)
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	if (left_height > right_height)
-		height = left_height;
-	else
-		height = right_height;
-	return (height);
+		return (left_height + 1);
+	return (right_height + 1);
 }
